4bd87d18a01edd5a3397cfae9f1eab280f071637
ItsDeidara <thisisarealemail@gmail.com>
2017-05-16 05:02:46 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Project_Sinner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.Halo = new MetroFramework.Controls.MetroTabPage();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.metroButton6 = new MetroFramework.Controls.MetroButton();
+            this.metroButton8 = new MetroFramework.Controls.MetroButton();
+            this.metroButton5 = new MetroFramework.Controls.MetroButton();
+            this.metroButton7 = new MetroFramework.Controls.MetroButton();
+            this.metroButton9 = new MetroFramework.Controls.MetroButton();
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.metroLink1 = new MetroFramework.Controls.MetroLink();
+            this.metroTabControl1.SuspendLayout();
+            this.Halo.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            this.metroTabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.Halo);
+            this.metroTabControl1.Controls.Add(this.metroTabPage1);
+            this.metroTabControl1.Controls.Add(this.metroTabPage2);
+            this.metroTabControl1.Location = new System.Drawing.Point(15, 52);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 2;
+            this.metroTabControl1.Size = new System.Drawing.Size(374, 280);
+            this.metroTabControl1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTabControl1.TabIndex = 2;
+            this.metroTabControl1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // Halo
+            // 
+            this.Halo.Controls.Add(this.metroButton7);
+            this.Halo.Controls.Add(this.metroButton5);
+            this.Halo.Controls.Add(this.metroButton4);
+            this.Halo.Controls.Add(this.metroButton3);
+            this.Halo.Controls.Add(this.metroButton2);
+            this.Halo.Controls.Add(this.metroButton1);
+            this.Halo.HorizontalScrollbarBarColor = true;
+            this.Halo.HorizontalScrollbarHighlightOnWheel = false;
+            this.Halo.HorizontalScrollbarSize = 10;
+            this.Halo.Location = new System.Drawing.Point(4, 38);
+            this.Halo.Name = "Halo";
+            this.Halo.Size = new System.Drawing.Size(366, 238);
+            this.Halo.TabIndex = 0;
+            this.Halo.Text = "Emulators";
+            this.Halo.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Halo.VerticalScrollbarBarColor = true;
+            this.Halo.VerticalScrollbarHighlightOnWheel = false;
+            this.Halo.VerticalScrollbarSize = 10;
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Location = new System.Drawing.Point(192, 88);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(167, 50);
+            this.metroButton4.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton4.TabIndex = 4;
+            this.metroButton4.Text = "PPSSPP (PSP)";
+            this.metroButton4.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton4.UseSelectable = true;
+            this.metroButton4.Click += new System.EventHandler(this.metroButton4_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(192, 23);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(167, 50);
+            this.metroButton3.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton3.TabIndex = 3;
+            this.metroButton3.Text = "NesBOX (NES)";
+            this.metroButton3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(3, 88);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(167, 50);
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton2.TabIndex = 2;
+            this.metroButton2.Text = "PSX-BOX (PS1)";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(3, 23);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(167, 50);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Chip8 (Chip8)";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.metroButton6);
+            this.metroTabPage1.Controls.Add(this.metroButton8);
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.HorizontalScrollbarSize = 10;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 38);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(366, 238);
+            this.metroTabPage1.TabIndex = 1;
+            this.metroTabPage1.Text = "Indie Games";
+            this.metroTabPage1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            this.metroTabPage1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.VerticalScrollbarSize = 10;
+            // 
+            // metroButton6
+            // 
+            this.metroButton6.Location = new System.Drawing.Point(196, 23);
+            this.metroButton6.Name = "metroButton6";
+            this.metroButton6.Size = new System.Drawing.Size(167, 50);
+            this.metroButton6.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton6.TabIndex = 7;
+            this.metroButton6.Text = "2D Zelda";
+            this.metroButton6.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton6.UseSelectable = true;
+            this.metroButton6.Click += new System.EventHandler(this.metroButton6_Click);
+            // 
+            // metroButton8
+            // 
+            this.metroButton8.Location = new System.Drawing.Point(3, 23);
+            this.metroButton8.Name = "metroButton8";
+            this.metroButton8.Size = new System.Drawing.Size(167, 50);
+            this.metroButton8.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton8.TabIndex = 5;
+            this.metroButton8.Text = "Dungeon Run";
+            this.metroButton8.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton8.UseSelectable = true;
+            this.metroButton8.Click += new System.EventHandler(this.metroButton8_Click);
+            // 
+            // metroButton5
+            // 
+            this.metroButton5.Location = new System.Drawing.Point(192, 144);
+            this.metroButton5.Name = "metroButton5";
+            this.metroButton5.Size = new System.Drawing.Size(167, 50);
+            this.metroButton5.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton5.TabIndex = 5;
+            this.metroButton5.Text = "Win64e10 (N64)";
+            this.metroButton5.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton5.UseSelectable = true;
+            this.metroButton5.Click += new System.EventHandler(this.metroButton5_Click);
+            // 
+            // metroButton7
+            // 
+            this.metroButton7.Location = new System.Drawing.Point(4, 144);
+            this.metroButton7.Name = "metroButton7";
+            this.metroButton7.Size = new System.Drawing.Size(167, 50);
+            this.metroButton7.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton7.TabIndex = 6;
+            this.metroButton7.Text = "VBA 10 (GBA)";
+            this.metroButton7.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton7.UseSelectable = true;
+            this.metroButton7.Click += new System.EventHandler(this.metroButton7_Click);
+            // 
+            // metroButton9
+            // 
+            this.metroButton9.Location = new System.Drawing.Point(139, 382);
+            this.metroButton9.Name = "metroButton9";
+            this.metroButton9.Size = new System.Drawing.Size(111, 40);
+            this.metroButton9.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton9.TabIndex = 7;
+            this.metroButton9.Text = "Check for updates";
+            this.metroButton9.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton9.UseSelectable = true;
+            this.metroButton9.Click += new System.EventHandler(this.metroButton9_Click);
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.FontSize = MetroFramework.MetroProgressBarSize.Tall;
+            this.metroProgressBar1.FontWeight = MetroFramework.MetroProgressBarWeight.Bold;
+            this.metroProgressBar1.HideProgressText = false;
+            this.metroProgressBar1.Location = new System.Drawing.Point(23, 318);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(354, 45);
+            this.metroProgressBar1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroProgressBar1.TabIndex = 8;
+            this.metroProgressBar1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.metroProgressBar1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroTabPage2
+            // 
+            this.metroTabPage2.Controls.Add(this.metroLink1);
+            this.metroTabPage2.HorizontalScrollbarBarColor = true;
+            this.metroTabPage2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.HorizontalScrollbarSize = 10;
+            this.metroTabPage2.Location = new System.Drawing.Point(4, 38);
+            this.metroTabPage2.Name = "metroTabPage2";
+            this.metroTabPage2.Size = new System.Drawing.Size(366, 238);
+            this.metroTabPage2.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTabPage2.TabIndex = 2;
+            this.metroTabPage2.Text = "About";
+            this.metroTabPage2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabPage2.VerticalScrollbarBarColor = true;
+            this.metroTabPage2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.VerticalScrollbarSize = 10;
+            // 
+            // metroLink1
+            // 
+            this.metroLink1.Location = new System.Drawing.Point(120, 31);
+            this.metroLink1.Name = "metroLink1";
+            this.metroLink1.Size = new System.Drawing.Size(122, 23);
+            this.metroLink1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroLink1.TabIndex = 2;
+            this.metroLink1.Text = "Created by @Red7s";
+            this.metroLink1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroLink1.UseSelectable = true;
+            this.metroLink1.UseStyleColors = true;
+            this.metroLink1.Click += new System.EventHandler(this.metroLink1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 445);
+            this.Controls.Add(this.metroProgressBar1);
+            this.Controls.Add(this.metroButton9);
+            this.Controls.Add(this.metroTabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "Project Sinner";
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroTabControl1.ResumeLayout(false);
+            this.Halo.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            this.metroTabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage Halo;
+        private MetroFramework.Controls.MetroButton metroButton7;
+        private MetroFramework.Controls.MetroButton metroButton5;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroButton metroButton6;
+        private MetroFramework.Controls.MetroButton metroButton8;
+        private MetroFramework.Controls.MetroButton metroButton9;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private MetroFramework.Controls.MetroLink metroLink1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+
+namespace Project_Sinner
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        private string test;
+        public Form1()
+        {
+            InitializeComponent();
+            Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Project Sinner"));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            {
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += Client_DownloadProgressChanged;
+                client.DownloadFileCompleted += Client_DownloadFileCompleted;
+                client.DownloadFileAsync(new Uri("https://www.dropbox.com/s/yh5bh4l566t8q77/CHIP8_XBOX_ONE_V3.rar?dl=1"), System.IO.Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.MyDoc‌​uments), "Project Sinner", "CHIP8.rar"));
+
+            }
+        }
+
+        private void Client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Complete!");
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            metroProgressBar1.Maximum = (int)e.TotalBytesToReceive / 100;
+            metroProgressBar1.Value = (int)e.BytesReceived / 100;
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            {
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += Client_DownloadProgressChanged1;
+                client.DownloadFileCompleted += Client_DownloadFileCompleted1;
+                client.DownloadFileAsync(new Uri("https://www.dropbox.com/s/ut124gswux57acs/NESBOX%20RELEASE2%20APPX%20PKG.rar?dl=1"), System.IO.Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.MyDoc‌​uments), "Project Sinner", "NesBOX.rar"));
+            }
+        }
+
+        private void Client_DownloadFileCompleted1(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Complete");
+        }
+
+        private void Client_DownloadProgressChanged1(object sender, DownloadProgressChangedEventArgs e)
+        {
+            metroProgressBar1.Maximum = (int)e.TotalBytesToReceive / 100;
+            metroProgressBar1.Value = (int)e.BytesReceived / 100;
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            {
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += Client_DownloadProgressChanged2;
+                client.DownloadFileCompleted += Client_DownloadFileCompleted2;
+                client.DownloadFileAsync(new Uri("https://www.dropbox.com/s/inxv8t8o0ov4g5x/psx-box-master.zip?dl=1"), System.IO.Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.MyDoc‌​uments), "Project Sinner", "psxbox.rar"));
+
+            }
+        }
+
+        private void Client_DownloadFileCompleted2(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Complete");
+        }
+
+        private void Client_DownloadProgressChanged2(object sender, DownloadProgressChangedEventArgs e)
+        {
+            metroProgressBar1.Maximum = (int)e.TotalBytesToReceive / 100;
+            metroProgressBar1.Value = (int)e.BytesReceived / 100;
+        }
+
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+            {
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += Client_DownloadProgressChanged3;
+                client.DownloadFileCompleted += Client_DownloadFileCompleted3;
+                client.DownloadFileAsync(new Uri("https://www.dropbox.com/s/nqgzpu8s311v6li/PPSSPP%20Release%20APPX%20PKG.rar?dl=1"), System.IO.Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.MyDoc‌​uments), "Project Sinner", "ppsspp.rar"));
+
+
+            }
+        }
+
+        private void Client_DownloadFileCompleted3(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Complete");
+        }
+
+        private void Client_DownloadProgressChanged3(object sender, DownloadProgressChangedEventArgs e)
+        {
+            metroProgressBar1.Maximum = (int)e.TotalBytesToReceive / 100;
+            metroProgressBar1.Value = (int)e.BytesReceived / 100;
+        }
+
+        private void metroButton7_Click(object sender, EventArgs e)
+        {
+            {
+
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += Client_DownloadProgressChanged4;
+                client.DownloadFileCompleted += Client_DownloadFileCompleted4;
+                client.DownloadFileAsync(new Uri("https://www.dropbox.com/s/wv82732vzc1c7xb/VBA10_1.22.197.0_Test.rar?dl=1"), System.IO.Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.MyDoc‌​uments), "Project Sinner", "VBA10.rar"));
+
+
+            }
+        }
+
+        private void Client_DownloadFileCompleted4(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Complete");
+        }
+
+        private void Client_DownloadProgressChanged4(object sender, DownloadProgressChangedEventArgs e)
+        {
+            metroProgressBar1.Maximum = (int)e.TotalBytesToReceive / 100;
+            metroProgressBar1.Value = (int)e.BytesReceived / 100;
+        }
+
+        private void metroButton5_Click(object sender, EventArgs e)
+        {
+            {
+
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += Client_DownloadProgressChanged5;
+                client.DownloadFileCompleted += Client_DownloadFileCompleted5;
+                client.DownloadFileAsync(new Uri("https://www.dropbox.com/s/4eom5r6wgcx5gbu/Win64e10%20PKG.rar?dl=1"), System.IO.Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.MyDoc‌​uments), "Project Sinner", "Win64e10.rar"));
+
+
+            }
+        }
+
+        private void Client_DownloadFileCompleted5(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Complete");
+        }
+
+        private void Client_DownloadProgressChanged5(object sender, DownloadProgressChangedEventArgs e)
+        {
+            metroProgressBar1.Maximum = (int)e.TotalBytesToReceive / 100;
+            metroProgressBar1.Value = (int)e.BytesReceived / 100;
+        }
+
+        private void metroButton8_Click(object sender, EventArgs e)
+        {
+            {
+                
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += Client_DownloadProgressChanged6;
+                client.DownloadFileCompleted += Client_DownloadFileCompleted6;
+                client.DownloadFileAsync(new Uri("https://www.dropbox.com/s/ny2bqesif910fuy/DungeonRUN.rar?dl=1"), System.IO.Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.MyDoc‌​uments), "Project Sinner", "DungeonRUN.rar"));
+
+            }
+        }
+
+        private void Client_DownloadFileCompleted6(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Complete");
+        }
+
+        private void Client_DownloadProgressChanged6(object sender, DownloadProgressChangedEventArgs e)
+        {
+            metroProgressBar1.Maximum = (int)e.TotalBytesToReceive / 100;
+            metroProgressBar1.Value = (int)e.BytesReceived / 100;
+        }
+
+        private void metroButton6_Click(object sender, EventArgs e)
+        {
+            {
+
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += Client_DownloadProgressChanged7;
+                client.DownloadFileCompleted += Client_DownloadFileCompleted7;
+                client.DownloadFileAsync(new Uri("https://www.dropbox.com/s/ny2bqesif910fuy/DungeonRUN.rar?dl=1"), System.IO.Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.MyDoc‌​uments), "Project Sinner", "2DZelda.rar"));
+
+
+            }
+        }
+
+        private void Client_DownloadFileCompleted7(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Complete");
+        }
+
+        private void Client_DownloadProgressChanged7(object sender, DownloadProgressChangedEventArgs e)
+        {
+            metroProgressBar1.Maximum = (int)e.TotalBytesToReceive / 100;
+            metroProgressBar1.Value = (int)e.BytesReceived / 100;
+        }
+
+        private void metroButton9_Click(object sender, EventArgs e)
+        {
+            string Version = "1.0.0.1";
+            string NewVersion = "1.0.0.2";
+            System.Net.WebClient request = new System.Net.WebClient();
+            string Update = request.DownloadString("https://pastebin.com/raw/Qj09Y1Ea");
+            if (Update.Contains(Version))
+            {
+                MessageBox.Show("Your software is up to date.");
+            }
+            else
+            {
+                MessageBox.Show("There is a new update, click OK to view the update.");
+                Process.Start("https://www.se7ensins.com/forums/threads/release-project-sinner-an-xbox-one-homebrew-utility-v1-0.1645782/");
+
+            }
+        }
+
+        private void metroLink1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://twitter.com/Red7s");
+        }
+    }
+}
